2192ad5858f36156d217bf3d6c1b47bafe05d64a
Ahmet Bozaci <ahmt9417@gmail.com>
2020-12-29 15:12:59 +0300

    Fix rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,50 +1,47 @@
 require 'nokogiri'
-require_relative "../lib/scraper.rb"
+require_relative '../lib/scraper.rb'
 require 'byebug'
 
-
-def variable
+def variables
   scraper = Scrapper.new
-  name = scraper.get_name
-  price = scraper.get_price
-  review = scraper.get_review
-  printer_list = Array.new 
-  name.count.times {|index| printer_list.push(Array.new)}
-  name.count.times {|index| printer_list[index].push(name[index],price[index],review[index])}
+  name = scraper.take_name
+  price = scraper.take_price
+  review = scraper.take_review
+  printer_list = []
+  name.count.times { |index| printer_list.push([]) }
+  name.count.times { |index| printer_list[index].push(name[index],price[index],review[index]) 
   printer_list
 end
 
 def display
   puts
-  puts "This scraper show FDM printers from Bangood store"
+  puts 'This scraper show FDM printers from Bangood store'
   puts
-  puts "If you want to sort them by price Press '1'"
+  puts 'If you want to sort them by price Press "1"'
   puts
-  puts "If you want to sort them by review Press '2'"
+  puts 'If you want to sort them by review Press "2"'
   puts
-  print "Your enter: "
+  print 'Your enter: '
 end
 
 def show_printers
   puts
-  puts "           PRINTER_NAME      |    PRICE | REVIEW"
-  puts "   --------------------------------------------"
+  puts '           PRINTER_NAME      |    PRICE | REVIEW'
+  puts '   --------------------------------------------'
 end
 
 def input
-  
   number = gets.chomp.to_i
-  puts "Please wait..."
-  numbers = [1,2]
-  variable
-  result = variable.sort_by{|list| list[number]}
+  puts 'Please wait...'
+  numbers = [1, 2]
+  variables
+  result = variable.sort_by{ |list| list[number] }
   show1 = result.size.times do |i|
     show_printers
-    puts "#{i+1} -- #{result[i]}"
+    puts "#{i + 1} -- #{result[i]}"
   end
-  numbers.include?(number) ? ( " #{show1} ") : (puts "you entered wrong number")
+  numbers.include?(number) ? ("#{show1}") : (puts "you entered wrong number")
 end
 
 display
 input
-
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,28 +6,26 @@ class Scrapper
   attr_accessor :parsed_page
 
   def initialize
-    url = "https://usa.banggood.com/Wholesale-Attribute-3D-Printer-c-10808-s-5347v13658.html"
+    url = 'https://usa.banggood.com/Wholesale-Attribute-3D-Printer-c-10808-s-5347v13658.html'
     unparsed_page = URI.open(url)
     @parsed_page = Nokogiri::HTML(unparsed_page)
   end
 
-  def get_name
-   parsed_page.css("a.title").map do |name|
-     name.text[0..25]
+  def take_name
+    @parsed_page.css('a.title').map do |name|
+      name.text[0..25]
     end
   end
 
-  def get_review
-    @parsed_page.css("a.review").map do |review|
-       review.text.gsub(/[A-Za-z]/,"").to_i
-     end
-  end
-
-  def get_price
-    @parsed_page.css("span.price").map do |price| 
-      price.text.gsub(/[A-Za-z,$,]/,"").to_f
+  def take_review
+    @parsed_page.css('a.review').map do |review|
+      review.text.gsub(/[A-Za-z]/, '').to_i
     end
   end
 
+  def take_price
+    @parsed_page.css('span.price').map do |price|
+      price.text.gsub(/[A-Za-z,$,]/, '').to_f
+    end
+  end
 end
-
